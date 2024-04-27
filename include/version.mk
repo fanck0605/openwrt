4d3596b76aa8aeedaf311cb6b0acc00c480a8bfb
Chuck Fan <fanck0605@qq.com>
2024-04-27 14:37:09 +0800

    version.mk: use OpenWrt/LEDE USTC Mirror
    Signed-off-by: Chuck Fan <fanck0605@qq.com>

--- include/version.mk
+++ include/version.mk
@@ -29,7 +29,7 @@ VERSION_CODE:=$(call qstrip,$(CONFIG_VERSION_CODE))
 VERSION_CODE:=$(if $(VERSION_CODE),$(VERSION_CODE),r29087-d9c5716d1d)
 
 VERSION_REPO:=$(call qstrip,$(CONFIG_VERSION_REPO))
-VERSION_REPO:=$(if $(VERSION_REPO),$(VERSION_REPO),https://downloads.openwrt.org/releases/24.10.5)
+VERSION_REPO:=$(if $(VERSION_REPO),$(VERSION_REPO),https://mirrors.ustc.edu.cn/openwrt/releases/24.10.5)
 
 VERSION_DIST:=$(call qstrip,$(CONFIG_VERSION_DIST))
 VERSION_DIST:=$(if $(VERSION_DIST),$(VERSION_DIST),OpenWrt)
